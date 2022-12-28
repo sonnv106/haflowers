85c35f4fcea72c39f557f716e7d2d564b0c5c858
DESKTOP-NVB8L6C\SonNV <sonnguyen106mh@gmail.com>
2022-12-28 11:46:00 +0700

    post method

--- src/index.js
+++ src/index.js
@@ -6,18 +6,29 @@ const app = express();
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json())
 
 app.engine('.hbs', engine({
   extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
   res.render('home');
 });
-
+app.get('/search', (req, res) => {
+  console.log(req.query)
+  res.render('search');
+});
+app.post('/news', (req, res) => {
+  console.log(req.body)
+  res.render('search');
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
